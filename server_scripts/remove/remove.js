b34154a78f6ea1f22f64d5a970b647af3f22b32c
Takoyaki <147054812+takoyakidath@users.noreply.github.com>
2025-05-03 09:44:41 +0900

    Update remove.js

--- server_scripts/remove/remove.js
+++ server_scripts/remove/remove.js
@@ -4,6 +4,7 @@ const removes = [
   "mekanism:resistive_heater", //なんで消すの...?熱源どうするの...?
   "industrialforegoing:ore_laser_base",
   "mekanismgenerators:wind_generator", //これもなぜ消すの？
+  "mob_grinding_utils:mob_swab"
 ];
 ServerEvents.recipes((event) => {
   removes.forEach((e) => event.remove({ output: e }));
